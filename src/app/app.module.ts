2993a20f46d626a3b77a6a78bdce7a849c472bb8
bxxstian <seb_faull@hotmail.co.uk>
2019-04-23 17:16:59 +0100

    Add Home Component and bike registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BikeService } from './services/bike.service';
-import { AdminComponent } from './components/admin/admin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent }     from './app.component';
+import { BikeService }      from './services/bike.service';
+import { AdminComponent }   from './components/admin/admin.component';
+import { HomeComponent }    from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    HttpClientModule
+    ReactiveFormsModule
   ],
   providers: [BikeService],
   bootstrap: [AppComponent]
--- src/app/services/bike.service.ts
+++ src/app/services/bike.service.ts
@@ -6,12 +6,21 @@ const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
-@Injectable({providedIn: 'root'})
+@Injectable(/*{providedIn: 'root'}*/)
 export class BikeService {
 
   constructor(private http:HttpClient) { }
 
   getBikes() {
     return this.http.get('/server/api/v1/bikes');
+  }
+
+  getBike(id: number) {
+    return this.http.get(`/server/api/v1/bikes/{id}`);
+  }
+
+  createBikeRegistration(bike) {
+    let body = JSON.stringify(bike);
+    return this.http.post('/server/api/v1/bikes', body, httpOptions);
   }
 }
